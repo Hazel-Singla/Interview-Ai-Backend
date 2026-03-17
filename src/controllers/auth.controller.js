48cf93d331158ca38120c843c211370cf08ef6b6
Hazel <hzlsingla@gmail.com>
2026-03-17 15:35:26 +0530

    Switch from cookie-based to token-based authentication for cross-domain support

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,16 +42,9 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 24 * 60 * 60 * 1000 // 1 day
-    })
-
-
     res.status(201).json({
         message: "User registered successfully",
+        token,
         user: {
             id: user._id,
             username: user.username,
@@ -93,14 +86,9 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 24 * 60 * 60 * 1000 // 1 day
-    })
     res.status(200).json({
         message: "User loggedIn successfully.",
+        token,
         user: {
             id: user._id,
             username: user.username,
